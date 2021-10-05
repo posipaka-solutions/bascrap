8fcd6f5f963a95f28c218f5bc83d357da2f61c43
DarkNeon135 <Beautifullife135>
2021-10-05 22:22:21 +0300

    edited parsing channel

--- internal/scraper/scraper.go
+++ internal/scraper/scraper.go
@@ -8,9 +8,10 @@ import (
 )
 
 const (
-	binanceChannelId = -1001146915409
+	//binanceChannelId = -1001146915409
 	//binanceChannelId    = -1001390705243 // testChannel
 	//binanceChannelId = -1001501948186 //Oleg
+	madNewsChannelId = -1001501948186 //-1001219306781
 )
 
 type ScrapHandler struct {
@@ -35,7 +36,7 @@ func New(tclient *client.Client) ScrapHandler {
 
 func (handler *ScrapHandler) GetLatestAnnounce() (announcement.Details, error) {
 	messages, err := handler.tdlibClient.GetChatHistory(&client.GetChatHistoryRequest{
-		ChatId:        binanceChannelId,
+		ChatId:        madNewsChannelId,
 		FromMessageId: 0,
 		Offset:        0,
 		Limit:         20,
@@ -45,7 +46,7 @@ func (handler *ScrapHandler) GetLatestAnnounce() (announcement.Details, error) {
 		return announcement.Details{}, err
 	}
 
-	announcedDetails, err := parseNewMessage(messages.Messages[0])
+	announcedDetails, err := parseMadNewsMessage(messages.Messages[0])
 	if err != nil {
 		return announcement.Details{}, err
 	}
@@ -58,41 +59,17 @@ func (handler *ScrapHandler) GetLatestAnnounce() (announcement.Details, error) {
 	return announcedDetails, nil
 }
 
-func parseNewMessage(message *client.Message) (announcement.Details, error) {
+func parseMadNewsMessage(message *client.Message) (announcement.Details, error) {
 	var messageText string
-	switch message.Content.MessageContentType() {
-	case client.TypeMessageVideo:
-		content, isOkay := message.Content.(*client.MessageVideo)
-		if !isOkay {
-			return announcement.Details{}, errors.New("[scraper] -> Casting of video message failed")
-		}
-		messageText = content.Caption.Text
-		break
-	case client.TypeMessagePhoto:
-		content, isOkay := message.Content.(*client.MessagePhoto)
-		if !isOkay {
-			return announcement.Details{}, errors.New("[scraper] -> Casting of photo message failed")
-		}
-		messageText = content.Caption.Text
-		break
-	case client.TypeMessageText:
-		content, isOkay := message.Content.(*client.MessageText)
-		if !isOkay {
-			return announcement.Details{}, errors.New("[scraper] -> Casting of text message failed")
-		}
-		messageText = content.Text.Text
-		break
-	default:
-		return announcement.Details{}, errors.New("[scraper] -> latest message in channel is neither video " +
-			"nor photo nor video")
+	content, isOkay := message.Content.(*client.MessageText)
+	if !isOkay {
+		return announcement.Details{}, errors.New("[scraper] -> Casting of text message failed")
 	}
+	messageText = content.Text.Text
+	messageText = messageText[:strings.Index(messageText, "\n")-1]
 
 	announcedDetails := announcement.Details{Header: messageText}
-	linkIdx := strings.Index(messageText, "https://")
-	if linkIdx != -1 {
-		announcedDetails.Header = messageText[:linkIdx-1]
-		announcedDetails.Link = messageText[linkIdx:]
-	}
 
 	return announcedDetails, nil
+
 }
